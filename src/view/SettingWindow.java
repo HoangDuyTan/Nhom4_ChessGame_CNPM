e655de0c4ada3e169984c194af0d803c50c47a85
nmhau2410 <23130096@st.hcmuaf.edu.vn>
2026-06-02 20:41:49 +0700

    fix: thau đổi màu săc bàn cờ bị mở thêm cửa sổ Setting

--- src/view/SettingWindow.java
+++ src/view/SettingWindow.java
@@ -88,9 +88,8 @@ public class SettingWindow extends JFrame {
                     break;
             }
 
-            gameWindow.dispose();
-
-            new GameWindow();
+            gameWindow.refreshTheme();
+            dispose();
         });
         contentPanel.add(createSettingCard("Màu sắc bàn cờ", "Thay đổi tông màu hiển thị của ô cờ và quân cờ.", colorBox));
 
