ecd241fb8637aa320ffc9b1004909ff84825d015
NguyenTruongGiang <23130082@st.hcmuaf.edu.vn>
2026-06-07 02:15:57 +0700

    test: unit test của lưu lịch sử nước đi khi đánh với máy

--- src/test/SaveManagerTest.java
+++ src/test/SaveManagerTest.java
@@ -1,11 +1,14 @@
 package test;
 
+import model.MoveLog;
+import model.Position;
 import model.SaveGameData;
 import org.junit.jupiter.api.Test;
 import view.SaveManager;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -41,4 +44,48 @@ public class SaveManagerTest {
         assertEquals(200, data.getSecondsElapsed()
         );
     }
+    /**
+     * Mục tiêu:
+     * Kiểm tra hệ thống tạo file save AI thành công.
+     */
+    @Test
+    void testSaveAIFileCreated() {
+        SaveManager.saveGameData(Color.WHITE, 100,new ArrayList<>(), true);
+        assertTrue(SaveManager.hasSaveFile(true));
+    }
+    /**
+     * Mục tiêu:
+     * Kiểm tra dữ liệu lượt chơi và thời gian
+     * được đọc lại chính xác từ file AI.
+     */
+    @Test
+    public void testAILoadGameData() {
+        SaveManager.saveGameData(Color.BLACK, 300, new ArrayList<>(), true);
+        SaveGameData data = SaveManager.loadGameData(true);
+        assertNotNull(data);
+        assertEquals(Color.BLACK, data.getTurn());
+        assertEquals(300, data.getSecondsElapsed());
+    }
+    /**
+     * Mục tiêu:
+     * Kiểm tra danh sách nước đi được lưu đúng
+     * khi chơi với AI.
+     *
+     * Dữ liệu mẫu:
+     * Trắng đi từ (6,4) -> (4,4)
+     * AI đi từ (1,4) -> (3,4)
+     */
+    @Test
+    public void testAISaveMoveHistory() {
+        List<MoveLog> moves = new ArrayList<>();
+        moves.add(new MoveLog(new Position(6, 4), new Position(4, 4), null, null, Color.WHITE));
+        moves.add(new MoveLog(new Position(1, 4), new Position(3, 4), null, null, Color.BLACK));
+        SaveManager.saveGameData(Color.WHITE, 500, moves, true);
+        SaveGameData data = SaveManager.loadGameData(true);
+        assertNotNull(data);
+        assertEquals(2, data.getMoves().size());
+        assertEquals("6,4,4,4", data.getMoves().get(0));
+        assertEquals("1,4,3,4", data.getMoves().get(1));
+    }
+
 }
